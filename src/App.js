9720697ae22808dc85bfaae8c775434cba914fa9
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-27 20:14:16 -0500

    ArtContainer properly renders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+import ArtContainer from './ArtContainer'
 
 function App() {
   return (
     <div className="App">
-
+      <ArtContainer />
     </div>
   );
 }
---
+++ src/ArtContainer/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class ArtContainer extends Component {
+	render() {
+		return <h2>Art Container</h2>
+	}
+}
