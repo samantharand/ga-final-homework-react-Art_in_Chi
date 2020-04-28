4217f6e9ea4d91bf1795b689a5b27ed1b6879c5c
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-27 20:34:04 -0500

    art shows up in state

--- src/ArtContainer/index.js
+++ src/ArtContainer/index.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react'
 
 export default class ArtContainer extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			art: []
+		}
+	}
+
+	componentDidMount() {
+		this.getArt()
+	}
+
+	getArt = async () => {
+		try {
+			const url = process.env.REACT_APP_API_URL + '/api/v1/art/'
+			// console.log('url', url);
+			const artResponse = await fetch(url)
+			// console.log(artResponse);
+			const artJson = await artResponse.json()
+			// console.log(artJson);
+			this.setState({
+				art: artJson.data
+			})
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
 	render() {
+		console.log(this.state);
 		return <h2>Art Container</h2>
 	}
 }
