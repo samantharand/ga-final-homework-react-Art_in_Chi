42996ad389a9474d0cfde6c73596e41dce51b133
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-04-27 20:52:37 -0500

    fun with semantic UI

--- src/ArtList/index.js
+++ src/ArtList/index.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Card, Image } from 'semantic-ui-react'
 
 export default function ArtList(props) {
 	const listedArt = props.art.map(art => {
 		return(
-			<p>{art.name}</p>
+			<Card>
+				<Card.Content>
+					<Image size='tiny' src='https://cdn2.iconfinder.com/data/icons/artist-3/64/painting-art-canvas-painter-512.png' />
+					<Card.Header> {art.name} </Card.Header>
+					<Card.Meta> {art.current_residence} </Card.Meta>
+					<Card.Description> {art.artist}, {art.year_made} </Card.Description>
+				</Card.Content>
+			</Card>
 		)}
 	)
 
 	return (
 		<>
 			<h4> ArtList </h4>
-			<div> {listedArt} </div>
+			<Card.Group centered={true}> {listedArt} </Card.Group>
 		</>
 	)
 }
